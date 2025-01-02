c7a14a5d1d2dab3f4d274ebcc2edd56bbfa9f8e2
doctawaald <88236819+doctawaald@users.noreply.github.com>
2025-01-02 20:10:12 +0100

    Update __init__.py

--- BotDisconnect/__init__.py
+++ BotDisconnect/__init__.py
@@ -1,4 +1,4 @@
-from .BotDisconnect import BotDisconnect
+from .BotDisconnect import ManageOtherBots
 
 async def setup(bot):
-    await bot.add_cog(BotDisconnect(bot))
+    await bot.add_cog(ManageOtherBots(bot))
