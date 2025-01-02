908070d7e5c9306aca445037b27b25a3e24a5eed
doctawaald <88236819+doctawaald@users.noreply.github.com>
2025-01-02 21:00:04 +0100

    Update disconnect_bots.py

--- BotDisconnect/disconnect_bots.py
+++ BotDisconnect/disconnect_bots.py
@@ -2,25 +2,36 @@ from redbot.core import commands
 from discord.ext import tasks
 import discord
 
-class DisconnectBots(commands.Cog):  # Verander 'Cog' naar 'commands.Cog'
+class DisconnectBots(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.check_bots.start()
+        self.bot_ready = False
+
+    async def cog_load(self):
+        """Zorg ervoor dat de taak pas start als de bot verbonden is."""
+        if self.bot.is_ready():
+            self.bot_ready = True
+            self.check_bots.start()
+        else:
+            # Wacht tot de bot volledig verbonden is voordat je de taak start
+            await self.bot.wait_until_ready()
+            self.bot_ready = True
+            self.check_bots.start()
 
     def cog_unload(self):
-        self.check_bots.cancel()
+        """Stop de taak als de cog wordt verwijderd."""
+        if self.bot_ready:
+            self.check_bots.cancel()
 
     @tasks.loop(minutes=5)
     async def check_bots(self):
-        for channel in self.bot.guilds[0].voice_channels:  # Verkrijgt de voice kanalen van de eerste guild
-            # Haal alle leden op die in het kanaal zijn
+        """Loop die bots uit kanalen verwijdert waar geen leden met de rol aanwezig zijn."""
+        for channel in self.bot.guilds[0].voice_channels:
             members_in_channel = [member for member in channel.members if member.guild_permissions.administrator is False]
 
-            # Check als er geen leden met de specifieke roleid 1188440119624085614 meer zijn
             if not any(role.id == 1188440119624085614 for member in members_in_channel for role in member.roles):
-                # Als er geen leden meer zijn, disconnect de bot uit het kanaal
                 for bot in channel.guild.members:
-                    if bot.bot:  # Controleer of het een bot is en als deze in het kanaal is
+                    if bot.bot:  # Als de bot in het kanaal is
                         if bot in channel.members:
                             await bot.move_to(None)  # Bot verlaat het kanaal
 
