493e94d30ab4dd9f4d072bdad00de993d5eb293f
doctawaald <88236819+doctawaald@users.noreply.github.com>
2025-01-02 20:24:12 +0100

    Update __init__.py

--- BotDisconnect/__init__.py
+++ BotDisconnect/__init__.py
@@ -1,4 +1,4 @@
-from .BotDisconnect import ManageOtherBots
+from .BotDisconnect import ManageOtherBots  # Importeer de class met de juiste naam
 
 async def setup(bot):
-    await bot.add_cog(ManageOtherBots(bot))
+    await bot.add_cog(ManageOtherBots(bot))  # Zorg ervoor dat de cog goed wordt toegevoegd
