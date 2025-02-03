e9ea8d44db2e67947b9ede27a1afa71b11ec2410
doctawaald <88236819+doctawaald@users.noreply.github.com>
2025-02-03 16:32:00 +0100

    Update __init__.py

--- Counter/__init__.py
+++ Counter/__init__.py
@@ -1,5 +1,4 @@
-def __init__(self, bot):
-    self.bot = bot
-    self.config = Config.get_conf(self, identifier=1234567890)
-    self.config.register_channel(counter=0, message_id=None)
+from .counter import Counter
 
+def setup(bot):
+    bot.add_cog(Counter(bot))
