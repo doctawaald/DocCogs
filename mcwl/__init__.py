e48ca62d3faa3479596defe15a127c4616f61281
doctawaald <88236819+doctawaald@users.noreply.github.com>
2025-02-24 17:16:24 +0100

    Update __init__.py

--- mcwl/__init__.py
+++ mcwl/__init__.py
@@ -1,8 +1,4 @@
 from .mc_wl import MCWhitelist
 
-__red_end_user_data_statement__ = "This cog does not store user data."
-
 async def setup(bot):
-    """Load the MCWhitelist cog"""
-    cog = MCWhitelist(bot)
-    await bot.add_cog(cog)
+    await bot.add_cog(MCWhitelist(bot))
