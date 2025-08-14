812f935122553077de076948d949951aaabec19a
doctawaald <88236819+doctawaald@users.noreply.github.com>
2025-08-14 11:11:37 +0200

    Update __init__.py

--- boozybank/__init__.py
+++ boozybank/__init__.py
@@ -1,6 +1,4 @@
 from .m00_core import BoozyBank
 
 async def setup(bot):
-    print("[BoozyBank] setup() called, adding cog...")
     await bot.add_cog(BoozyBank(bot))
-    print("[BoozyBank] cog added.")
