d9709b0ad6c3939348b27dc95cd903a406c01fc0
doctawaald <88236819+doctawaald@users.noreply.github.com>
2025-12-31 13:12:28 +0100

    Create __init__.py

---
+++ gamenight/__init__.py
@@ -0,0 +1,6 @@
+from .gamenight import setup
+
+__red_end_user_data_statement__ = "Deze cog slaat tijdelijk User IDs op in het geheugen voor het stemmen, maar bewaart niets permanent op schijf."
+
+async def setup(bot):
+    await setup(bot)
